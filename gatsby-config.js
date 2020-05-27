ac4b7afa67b3e32eb414e5599ab712ae8e63d1ff
Karthik Raveendran <karthik.panicker@gmail.com>
2020-05-28 01:06:46 +0530

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
+    title: `Gias Personal Website`,
+    author: `Karthik Raveendran`,
     description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://gias.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `karthikraveendran`,
     },
   },
   plugins: [
